448bca9a2bd40dc0c7c460ecf9d89400f89ce117
andrew-kam <andrewkamvik@gmail.com>
2023-04-30 16:30:25 +0300

    refactor

--- app/src/androidTest/java/testing/wiki/pages/CustomizeFeedPage.kt
+++ app/src/androidTest/java/testing/wiki/pages/CustomizeFeedPage.kt
@@ -6,24 +6,23 @@ import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.Until
-import org.junit.Assert
+import org.junit.Assert.assertTrue
 
 class CustomizeFeedPage : BasePage() {
 
-    private val matcherCheckBox = "org.wikipedia.alpha:id/feed_content_type_checkbox"
+    private val bySelectorCheckBox = "org.wikipedia.alpha:id/feed_content_type_checkbox"
     private val matcherRecycle = withId(R.id.content_types_recycler)
 
     fun checkAllCustomizeFeedBoxesAreChecked() {
         val timeOut = 3000L
         val uiDevice = UiDevice.getInstance(getInstrumentation())
 
-        uiDevice.wait(Until.hasObject(By.res(matcherCheckBox)), timeOut)
-        val listCheckBox = uiDevice.findObjects(By.res(matcherCheckBox))
+        uiDevice.wait(Until.findObjects(By.res(bySelectorCheckBox)), timeOut)
 
-        Assert.assertFalse("Check-boxes were not found!", listCheckBox.isEmpty())
-        for (checkBox in listCheckBox) {
-            Assert.assertTrue("Check-box isn`t checked!", checkBox.isChecked)
-        }
+        uiDevice.findObjects(By.res(bySelectorCheckBox))
+            .forEach {
+                assertTrue("Check-box isn`t checked!", it.isChecked)
+            }
     }
 
     fun swipeUp() =
